7d687994427fae24a7036dd22f07269f11ab91ad
Nicholas Zimmerman <33072270+nzimmerman3@users.noreply.github.com>
2019-06-07 13:43:23 -0700

    Update server.c

--- server.c
+++ server.c
@@ -6,68 +6,226 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/time.h>
+#include <signal.h>
 
 #define BUFSIZE 524
 
+struct packet
+{
+	int16_t seq_num;
+	int16_t ack_num;
+	int16_t ack;
+	int16_t syn;
+	int16_t fin;
+	int16_t filler;
+
+	//char data[PAYLOAD];
+	char* data;
+};
+
+int filenum;
+FILE *fptr;
+char* filename;
+
+void handle(int sig)
+{
+	fclose(fptr);
+	fptr = fopen(filename, "w");
+	fprintf(fptr, "%s", "C-c");
+	fclose(fptr);
+	exit(0);
+}
+
+
 int main(int argc, char **argv)
 {
-    int sockfd;
-    int port;
-    struct sockaddr_in serveraddr;
-    struct sockaddr_in clientaddr;
-    int message_size;
-    int addr_len;char buffer[BUFSIZE];
+	signal(SIGTERM, handle); //shell command kill
+	signal(SIGQUIT, handle); //C - \
 
-    int filenum = 0;
-    char* filename;
+	int sockfd;
+	int port;
+	struct sockaddr_in serveraddr;
+	struct sockaddr_in clientaddr;
+	int message_size;
+	int addr_len;
+	char buffer[BUFSIZE];
 
-    FILE *fptr;
+	struct packet ps;
+	struct packet pr;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "ERROR:Please provide a valid port\n");
-        exit(1);
-    }
-    port = atoi(argv[1]);
+	filenum = 0;
+	
+
+	int16_t nextSeq;
+	int16_t currAck;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "ERROR:Please provide a valid port\n");
+		exit(1);
+	}
+	port = atoi(argv[1]);
 
 
-    //create socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	//create socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    if (sockfd < 0)
-    {
-        perror("ERROR:creating socket\n");
-        exit(1);
-    }
+	if (sockfd < 0)
+	{
+		perror("ERROR:creating socket\n");
+		exit(1);
+	}
 
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    memset(&clientaddr, 0, sizeof(clientaddr));
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	memset(&clientaddr, 0, sizeof(clientaddr));
 
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    serveraddr.sin_port = htons(port);
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	serveraddr.sin_port = htons(port);
 
-    if (bind(sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
-    {
-        perror("ERROR:binding socket\n");
-        exit(1);
-    }
+	if (bind(sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+	{
+		perror("ERROR:binding socket\n");
+		exit(1);
+	}
 
-    addr_len = sizeof(clientaddr);
 
-    //server loop
-    while(1)
-    {
-        message_size = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *) &clientaddr, &addr_len);
-        sprintf(filename, "%d", filenum);
-        fptr = fopen(filename, "w");
-        if (fptr == NULL)
-        {
-            perror("ERROR:opening file\n");
-            exit(1);
-        }
-        fprintf(fptr, "%s", buffer);
-        fclose(fptr);
-    }
+	addr_len = sizeof(clientaddr);
+
+	
+	while(1)
+	{
+		
+
+		memset((char *) &ps, 0, sizeof(ps));
+		memset((char *) &pr, 0, sizeof(pr));
+		message_size = recvfrom(sockfd, &pr, sizeof(pr), 0, (struct sockaddr *) &clientaddr, &addr_len);
+		
+		if(pr.syn == 0)
+		{
+			perror("ERROR:syn not set\n");
+		}
+		filenum += 1;
+		sprintf(filename, "%d", filenum);
+		fptr = fopen(filename, "w");
+		if (fptr == NULL)
+		{
+			perror("ERROR:opening file\n");
+			exit(1);
+		}
+
+
+		//set seq num
+		ps.seq_num = rand() % 25601;
+
+		//set ack num
+		if(pr.seq_num == 25600)
+			ps.ack_num = 0;
+		else
+			ps.ack_num = pr.seq_num + 1;
+
+
+		//set flags
+		ps.ack = 1;
+		ps.syn = 1;
+		ps.fin = 0;
+
+		currAck = ps.ack_num;
+		nextSeq = ps.seq_num + sizeof(ps);
+		if(nextSeq > 25600)
+				nextSeq -= 25600;
+
+		//send SYN response
+		if(sendto(sockfd, &ps, sizeof(ps), 0, (const struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0)
+		{
+			perror("ERROR:sending message\n");
+			exit(1);
+		} 
+
+		memset((char *) &pr, 0, sizeof(pr));
+
+		struct timeval tv;
+		tv.tv_sec = 0;
+		tv.tv_usec = 10000000;
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
+		{
+			perror("ERROR:setting timeout\n");
+		}
+
+		message_size = recvfrom(sockfd, &pr, sizeof(pr), 0, (struct sockaddr *) &clientaddr, &addr_len);
+		
+		while(pr.fin != 0 && message_size >= 0)
+		{
+			//make sure message acked last sent and seq num is expected
+			if(pr.ack_num < nextSeq || pr.seq_num != currAck)
+			{
+				//need to retransmit
+				if(sendto(sockfd, &ps, sizeof(ps), 0, (const struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0)
+				{
+					perror("ERROR:sending message\n");
+					exit(1);
+				} 	
+				
+			}
+			else
+			{
+				//copy data from client to file
+				fwrite(pr.data, 1, sizeof(pr.data), fptr);
+
+				memset((char *) &ps, 0, sizeof(ps));
+				//send message acking current message
+				ps.seq_num = nextSeq;
+				ps.ack_num = pr.seq_num + message_size;
+				ps.ack = 1;
+				ps.syn = 0;
+				ps.fin = 0;
+
+				nextSeq += sizeof(ps);
+				if(nextSeq > 25600)
+					nextSeq -= 25600;
+			}
+			
+			
+
+			
+					
+			//get new message
+			memset((char *) &pr, 0, sizeof(pr));
+			message_size = recvfrom(sockfd, &pr, sizeof(pr), 0, (struct sockaddr *) &clientaddr, &addr_len);
+		}
+		//recieved packet with FIN or timeout
+
+		if(message_size < 0)
+		{
+			//FIN
+			memset((char *) &ps, 0, sizeof(ps));
+			ps.seq_num = nextSeq;
+			ps.ack_num = pr.seq_num + message_size;
+			ps.ack = 1;
+			ps.syn = 0;
+			ps.fin = 1;
+
+			nextSeq += sizeof(ps);
+			if(nextSeq > 25600)
+				nextSeq -= 25600;
+
+			do
+			{
+				if(sendto(sockfd, &ps, sizeof(ps), 0, (const struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0)
+				{
+					perror("ERROR:sending message\n");
+					exit(1);
+				} 	
+
+				memset((char *) &pr, 0, sizeof(pr));
+				message_size = recvfrom(sockfd, &pr, sizeof(pr), 0, (struct sockaddr *) &clientaddr, &addr_len);
+			}while(pr.ack_num != nextSeq);
+		}
+		
+		
+		fclose(fptr);
+	}
 
 }
